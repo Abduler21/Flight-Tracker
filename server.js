4fb68b6c439d17c341ee754243a9ffdec2f2b9fb
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-01-29 18:28:10 -0500

    added config file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+//Dependencies//
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3005;
 
+//Middleware//
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
