cdbe983cb1c76f6dd9bc2b25b327a2df1ea0a106
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-01-29 17:25:43 -0500

    started Node.js server

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3005;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-// Starting up server for node (universal code)//
 
+// Starting up server for node (universal code)//
 app.listen(PORT, function () {
   console.log(
     "==>   Listening on port %s. Visit http://localhost:%s/ in your browser.",
