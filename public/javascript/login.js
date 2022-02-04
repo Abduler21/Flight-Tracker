dd63e43de695f5188e2bac94dbbb34b67c598c0c
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-02-04 00:21:16 -0500

    login redirects to homepage

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -18,7 +18,7 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/homepage");
     } else {
       alert(response.statusText);
     }
