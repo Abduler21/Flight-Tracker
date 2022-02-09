e1b07eaefb7a200be497109ceb65207a1334cb3a
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-02-08 22:17:28 -0500

    added flights model to flight js

--- Models/Flights.js
+++ Models/Flights.js
@@ -12,4 +12,30 @@ Flights.init({
   totalAmount: {
     type: DataTypes.DECIMAL,
   },
+
+  passenger_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  cabin_class: {
+    type: DataTypes.STRING,
+  },
+  originName: { type: DataTypes.STRING },
+
+  timeZone: { type: DataTypes.STRING },
+
+  operating_carrier: { type: DataTypes.STRING },
+
+  destinationName: { type: DataTypes.STRING },
+
+  departing_at: { type: DataTypes.DATE },
+
+  arriving_at: { type: DataTypes.DATE },
+
+  duration: { type: DataTypes.STRING },
+
+  origin_city: { type: DataTypes.STRING },
+
+  destination_city: { type: DataTypes.STRING },
 });
