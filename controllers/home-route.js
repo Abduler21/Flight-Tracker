9003256e521c1b6f65e6bf62b566e61d3a13887e
souhila boukari <suhamimisou14@gmail.com>
2022-02-09 23:37:21 -0500

    addig imge and css file

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const axios = require("axios");
 const { default: axios } = require("axios");
 const sequelize = require("../config/connection");
 const { Post, User, Comment, Vote } = require("../models");
