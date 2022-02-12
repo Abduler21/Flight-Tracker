4771798d9b56f922b3e45de11124fc7f718c771d
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-02-12 15:33:00 -0500

    added duration onto webpage after searching for flights

--- public/javascript/duffel_API.js
+++ public/javascript/duffel_API.js
@@ -18,13 +18,25 @@ $(document).ready(function () {
       .then((data) => {
         console.log("The data from duffel is", data);
         const flights = $("#flight-list");
-        for( let i=0; i<data.data.offers.offers.length; i++ ){
-          let li = $("<li>");
-          li.text(data.data.offers.offers[i].owner.name)
-          flights.append(li)
-  
-        } 
-        
+        for (let i = 0; i < data.data.offers.offers.length; i++) {
+          let li = $("<li>"); // this li will hold each flight info
+
+          let flightDuration = $("<p>"); // step 1. make variable for html
+          flightDuration.text(
+            // step 2. make text of html variable the data you want from API
+            data.data.offers.offers[i].slices[0].duration.split("T")[1]
+          );
+
+          let CarrierName = $("<p>"); // hold name of carrier(ex: turkish airlines)
+          CarrierName.text(data.data.offers.offers[i].owner.name);
+
+          // step 3. put all the data inside the li
+          li.append(flightDuration);
+          li.append(CarrierName);
+
+          // step 4. push the li into the flight-list ol in html
+          flights.append(li);
+        }
 
         return data;
       })
