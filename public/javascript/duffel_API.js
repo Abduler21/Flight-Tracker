1e76a153bbe78a520a245afadb4adaf6a50834a5
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-02-13 03:08:17 -0500

    added validation for flight searches

--- public/javascript/duffel_API.js
+++ public/javascript/duffel_API.js
@@ -5,6 +5,18 @@ $(document).ready(function () {
 
   // function to search and display flights from api
   const getFlights = (from, to, date, passenger, classType) => {
+    // the current date
+    // this will be used to compare with the date the user wants to book a flight
+    let calender = new Date();
+    let day = calender.getDate();
+    let month = calender.getMonth() + 1;
+    let year = calender.getFullYear();
+
+    let currentDate = `${year}-${month}-${day}`;
+
+    console.log(
+      `The current date is: ${currentDate} and the date the user chose is: ${date}`
+    );
     let flightOffer = {
       cabin: classType,
       departure_date: date,
@@ -13,71 +25,86 @@ $(document).ready(function () {
       type: passenger,
     };
 
-    // Calling the lookup route to get flights from api
-    $.post("/api/flights/lookup", flightOffer, function () {
-      console.log("Called route to duffel API");
-    })
-      .then((data) => {
-        console.log("The data from duffel is", data);
-        const flights = $("#flight-list");
-        for (let i = 0; i < data.data.offers.offers.length; i++) {
-          let li = $("<li>"); // this li will hold each flight info
-          li.attr("id", data.data.offers.offers[i].id);
-
-          // FLIGHT DURATION
-          let flightDuration = $("<p>"); // step 1. make variable for html
-          flightDuration.text(
-            // step 2. make text of html variable the data you want from API
-            data.data.offers.offers[i].slices[0].duration.split("T")[1]
-          );
-          flightDuration.attr("id", "flight-duration");
-
-          // ORIGIN NAME
-          let originName = $("<p>");
-          originName.text(
-            data.data.offers.offers[i].slices[0].origin.city_name
-          );
-          originName.attr("id", "origin-name");
-
-          // DESTINATION NAME
-          let destinationName = $("<p>");
-          destinationName.text(
-            data.data.offers.offers[i].slices[0].destination.city_name
-          );
-          destinationName.attr("id", "destination-name");
-
-          // CARRIER NAME
-          let CarrierName = $("<p>"); // hold name of carrier(ex: turkish airlines)
-          CarrierName.text(data.data.offers.offers[i].owner.name);
-          CarrierName.attr("id", "carrier-name");
-
-          // COST
-          let cost = $("<p>");
-          cost.text("$" + data.data.offers.offers[i].total_amount);
-          cost.attr("id", "cost-amount");
-
-          // BOOK FLIGHT BUTTON
-          let bookButton = $("<button>");
-          bookButton.text("Book Flight");
-          bookButton.addClass("btn btn-success");
-          bookButton.attr("id", "book-btn");
-
-          // step 3. put all the data inside the li
-          li.append(flightDuration);
-          li.append(CarrierName);
-          li.append(originName);
-          li.append(destinationName);
-          li.append(cost);
-          li.append(bookButton);
-
-          // step 4. push the li into the flight-list ol in html
-          flights.append(li);
-        }
-        return data;
+    // if any of the inputs are empty then dont search for a flight
+    if (
+      from === "" ||
+      to === "" ||
+      date === "" ||
+      passenger === "" ||
+      classType === ""
+    ) {
+      alert(
+        "Please make sure to fill in all the information to search for flights!"
+      );
+    } else if (from === to) {
+      alert("Please make sure your From and To are not the same!");
+    } else {
+      // Calling the lookup route to get flights from api
+      $.post("/api/flights/lookup", flightOffer, function () {
+        console.log("Called route to duffel API");
       })
-      .catch((err) => {
-        console.log(JSON.stringify(err));
-      });
+        .then((data) => {
+          console.log("The data from duffel is", data);
+          const flights = $("#flight-list");
+          for (let i = 0; i < data.data.offers.offers.length; i++) {
+            let li = $("<li>"); // this li will hold each flight info
+            li.attr("id", data.data.offers.offers[i].id);
+
+            // FLIGHT DURATION
+            let flightDuration = $("<p>"); // step 1. make variable for html
+            flightDuration.text(
+              // step 2. make text of html variable the data you want from API
+              data.data.offers.offers[i].slices[0].duration.split("T")[1]
+            );
+            flightDuration.attr("id", "flight-duration");
+
+            // ORIGIN NAME
+            let originName = $("<p>");
+            originName.text(
+              data.data.offers.offers[i].slices[0].origin.city_name
+            );
+            originName.attr("id", "origin-name");
+
+            // DESTINATION NAME
+            let destinationName = $("<p>");
+            destinationName.text(
+              data.data.offers.offers[i].slices[0].destination.city_name
+            );
+            destinationName.attr("id", "destination-name");
+
+            // CARRIER NAME
+            let CarrierName = $("<p>"); // hold name of carrier(ex: turkish airlines)
+            CarrierName.text(data.data.offers.offers[i].owner.name);
+            CarrierName.attr("id", "carrier-name");
+
+            // COST
+            let cost = $("<p>");
+            cost.text("$" + data.data.offers.offers[i].total_amount);
+            cost.attr("id", "cost-amount");
+
+            // BOOK FLIGHT BUTTON
+            let bookButton = $("<button>");
+            bookButton.text("Book Flight");
+            bookButton.addClass("btn btn-success");
+            bookButton.attr("id", "book-btn");
+
+            // step 3. put all the data inside the li
+            li.append(flightDuration);
+            li.append(CarrierName);
+            li.append(originName);
+            li.append(destinationName);
+            li.append(cost);
+            li.append(bookButton);
+
+            // step 4. push the li into the flight-list ol in html
+            flights.append(li);
+          }
+          return data;
+        })
+        .catch((err) => {
+          console.log(JSON.stringify(err));
+        });
+    }
   };
 
   $("#search-btn").on("click", (event) => {
