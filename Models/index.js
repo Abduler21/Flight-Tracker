ebf6ade234293edc0d9ed1062fb2b190f06b463f
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-02-13 23:49:38 -0500

    wow

--- Models/index.js
+++ Models/index.js
@@ -1,9 +1,9 @@
-const PORT= 3005
-const axios = require('axios')
-const express = require('express')
-const app = express()
+const PORT = 3005;
+const axios = require("axios");
+const express = require("express");
+const app = express();
 
-const User = require("./Users");
-const Flights = require("./Flights");
+const User = require("./users");
+const Flights = require("./flights");
 
 module.exports = { User, Flights };
