7e33448d1aa6c761ae04fb51d3db3d273cf99d29
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-02-14 00:00:35 -0500

    wow

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,14 @@
 const Sequelize = require("sequelize");
-const dotenv = require("dotenv");
 
-dotenv.config();
+require("dotenv").config();
 
-module.exports = new Sequelize(
-  process.env.DATABASE,
-  process.env.MYSQL_USERNAME,
-  process.env.MYSQL_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    operatorsAliases: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }
-);
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    });
+
+module.exports = sequelize;
