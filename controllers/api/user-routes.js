3d68426faf32e593889f8c208c5de5addb008e5f
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-02-14 20:13:00 -0500

    fixed file names

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models/Users");
+const { User } = require("../../models/users");
 
 // get all users
 router.get("/", (req, res) => {
